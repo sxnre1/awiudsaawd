72f8adbc8f7bbd7d84bbd116aee4084585c282c2
unknown <senleodaaauwa@gmail.com>
2025-11-09 03:41:23 +0900

    Initial commit2

--- web.py
+++ web.py
@@ -86,4 +86,5 @@ def autocomplete():
         return jsonify({"success": False, "error": str(e)}), 500
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
